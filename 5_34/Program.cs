c66d630b22df3c3ea0cc9a693916fbf086ab153f
Anna <afrodita-kts@mail.ru>
2022-08-08 19:39:35 +0300

    задача 34

--- 5_34/Program.cs
+++ 5_34/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+int []array=new int[8];
+int count=0;
+for (int n=0; n<8; n++) 
+    array[n]=new Random().Next(100,1000);
+Console.WriteLine("[" +string.Join(',' , array)+ "]");  
+if (array[n]%2==0)
+    count++;
+    Console.WriteLine(count);
+
+
